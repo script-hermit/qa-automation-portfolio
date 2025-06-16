9d93e858556c3db3a8da7d0d7d94868b213d220d
script-hermit <scriptermit@gmail.com>
2025-06-16 13:42:33 -0500

    Enhance API tests with Allure reporting and caching for improved test management

--- api-tests/src/test/java/WikipediaApiTest.java
+++ api-tests/src/test/java/WikipediaApiTest.java
@@ -1,5 +1,9 @@
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
+import io.qameta.allure.Attachment;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
@@ -7,20 +11,65 @@ import static org.hamcrest.Matchers.*;
 
 public class WikipediaApiTest {
 
-    @Test
-    public void searchForAppium() {
+    @DataProvider(name = "searchTerms")
+    public Object[][] searchTerms() {
+        return new Object[][] {
+            {"Appium"},
+            {"TestNG"},
+            {"Selenium"}
+        };
+    }
+
+    @Test(dataProvider = "searchTerms", description = "Verify Wikipedia API returns results for various search terms")
+    @Description("Searches Wikipedia API for each provided term and verifies that results are returned.")
+    public void searchForTerm(String term) {
         RestAssured.baseURI = "https://en.wikipedia.org/w/api.php";
 
-        given()
-            .queryParam("action", "query")
-            .queryParam("list", "search")
-            .queryParam("srsearch", "Appium")
-            .queryParam("format", "json")
-        .when()
-            .get()
-        .then()
-            .statusCode(200)
-            .contentType(ContentType.JSON)
-            .body("query.search[0].title", containsString("Appium"));
+        String response =
+            given()
+                .queryParam("action", "query")
+                .queryParam("list", "search")
+                .queryParam("srsearch", term)
+                .queryParam("format", "json")
+                .log().all()
+            .when()
+                .get()
+            .then()
+                .log().all()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("query.search[0].title", containsString(term))
+                .extract().asString();
+
+        attachResponseToAllure(response);
+    }
+
+    @Test(description = "Verify Wikipedia API returns no results for gibberish search term")
+    @Description("Searches Wikipedia API for a gibberish term and verifies that no results are returned.")
+    public void searchForNoResults() {
+        RestAssured.baseURI = "https://en.wikipedia.org/w/api.php";
+
+        String response =
+            given()
+                .queryParam("action", "query")
+                .queryParam("list", "search")
+                .queryParam("srsearch", "asdkfjhasdkjfhakjsdhf")
+                .queryParam("format", "json")
+                .log().all()
+            .when()
+                .get()
+            .then()
+                .log().all()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("query.search.size()", equalTo(0))
+                .extract().asString();
+
+        attachResponseToAllure(response);
+    }
+
+    @Attachment(value = "API Response", type = "application/json")
+    public String attachResponseToAllure(String response) {
+        return response;
     }
 }
