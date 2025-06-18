3288a39ab3531814dc0f617f1bb97e1c403cc8d0
script-hermit <scriptermit@gmail.com>
2025-06-17 22:19:56 -0500

    chore: remove manual installation of Appium settings APK from emulator test script

--- .github/scripts/android-emulator-test.sh
+++ .github/scripts/android-emulator-test.sh
@@ -20,13 +20,7 @@ adb shell settings put global transition_animation_scale 0.0
 adb shell settings put global animator_duration_scale 0.0
 adb shell settings put secure spell_checker_enabled 0
 
-SETTINGS_APK="/home/runner/.appium/node_modules/appium-uiautomator2-driver/node_modules/io.appium.settings/apks/settings_apk-debug.apk"
-if [ -f "$SETTINGS_APK" ]; then
-  echo "Attempting manual install of Appium settings APK"
-  adb install -r "$SETTINGS_APK" || true
-else
-  echo "Settings APK not found, skipping manual install"
-fi
+
 
 cd mobile-tests && mvn clean test
 TEST_EXIT_CODE=$?
